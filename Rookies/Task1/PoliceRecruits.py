1fa442dc33934aced20e5de43000b1e58857a300
Ma7moudEGY <mahmoudfatahalla@gmail.com>
2024-11-30 02:25:28 +0200

    Solving The Police Recruits Problem

--- Rookies/Task1/PoliceRecruits.py
+++ Rookies/Task1/PoliceRecruits.py
@@ -1,29 +1,22 @@
 length = input("")
 Events = input("").split()
 
-recruits = 0
-crimes = 0
-
 def Untreatd_Crimes(events : list):
     recruits = 0
-    crimes = 0
     untreated = 0
 
     res = list(map(int, events))
 
     for event in res:
-        if recruits > 0 and event == -1:
-            recruits -= 1
-            continue
-
         if event == -1:
-            crimes += 1
+            if recruits > 0:
+                recruits -= 1
+            else:
+                untreated += 1
         else:
             recruits += event
 
-        if crimes >= recruits:
-            if untreated + 1 <= crimes:
-                untreated += 1
+    print(untreated)
 
     print(untreated)
 
