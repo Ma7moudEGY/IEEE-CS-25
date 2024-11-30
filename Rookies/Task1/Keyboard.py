18f9ef715fbbe3ee7d0ed32fb28fca37ee3a5982
Ma7moudEGY <mahmoudfatahalla@gmail.com>
2024-11-30 14:38:56 +0200

    Solving the Keyboad Problem

--- Rookies/Task1/Keyboard.py
+++ Rookies/Task1/Keyboard.py
@@ -1,1 +1,19 @@
+shift = input("")
 blind_txt = input("")
+
+def OriginalText(text):
+    orignal = []
+    keyboard = "qwertyuiopasdfghjkl;zxcvbnm,./"
+
+    for i in range(len(text)):
+        if shift == "R":
+            j = keyboard.index(text[i])
+            orignal.append(keyboard[j-1])
+
+        else:
+            j = keyboard.index(text[i])
+            orignal.append(keyboard[j+1])
+
+    print(''.join(orignal))
+
+OriginalText(blind_txt)
